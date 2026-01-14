3023b624ed7cfbeb1080059c5158d18bdd2279c9
cire <121199085+sage1995@users.noreply.github.com>
2026-01-15 00:57:07 +0200

    MainActivity.kt

--- app/src/main/Java/com/example/floatingkeys/MainActivity.kt
+++ app/src/main/Java/com/example/floatingkeys/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.floatingkeys
 
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
 import androidx.appcompat.app.AppCompatActivity
@@ -9,14 +10,32 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        
+        // check for permission
         if (!Settings.canDrawOverlays(this)) {
-            startActivity(Intent(
-                Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                Uri.parse("package:$packageName")
-            ))
+            val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:$packageName"))
+            startActivityForResult(intent, 0)
         } else {
-            startService(Intent(this, FloatingService::class.java))
-            finish()
+            startFloatingService()
+        }
+    }
+
+    // Determine safe way to start service
+    private fun startFloatingService() {
+        val intent = Intent(this, FloatingService::class.java)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(intent)
+        } else {
+            startService(intent)
+        }
+        finish() // Close activity immediately
+    }
+    
+    // Retry if user just granted permission
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (Settings.canDrawOverlays(this)) {
+            startFloatingService()
         }
     }
 }
